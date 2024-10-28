620615df39c7013f70398b3b55c3b46bec3395b0
Vaidul1s <vaidas.ad25@gmail.com>
2024-10-28 14:09:52 +0200

    dgd

---
+++ 10/app.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+const leftClick = document.querySelector('.leftClick');
+const rightClick = document.querySelector('.rightClick');
+
+console.log(slides, typeof slides);
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+rightClick.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
+
+leftClick.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
+
+showSlide(currentSlide);
