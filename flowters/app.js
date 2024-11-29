293ac71315d7f42effd14b3209114c61cc5b9468
Vaidul1s <vaidas.ad25@gmail.com>
2024-11-29 10:33:56 +0200

    11.29

--- flowters/app.js
+++ flowters/app.js
@@ -4,6 +4,14 @@ const phrases = [
     { id: 'phrase3', x: 400, y: 300, dx: -1, dy: 2 },
     { id: 'phrase4', x: 100, y: 400, dx: -2, dy: -1 },
     { id: 'phrase5', x: 300, y: 50, dx: 2, dy: 2 },
+    { id: 'phrase6', x: 200, y: 50, dx: 4, dy: 2 },
+    { id: 'phrase7', x: 150, y: 150, dx: 2, dy: 3 },
+    { id: 'phrase8', x: 250, y: 100, dx: -2, dy: 2 },
+    { id: 'phrase9', x: 50, y: 250, dx: -1, dy: -3 },
+    { id: 'phrase10', x: 350, y: 350, dx: 1, dy: -2 },
+    { id: 'phrase11', x: 400, y: 150, dx: -4, dy: 1 },
+    { id: 'phrase12', x: 200, y: 400, dx: 1, dy: -1 },
+
 ];
 
 function movePhrases() {
