97ac5aa5362e1b26e2cfafdef66912816e94a300
Vaidul1s <vaidas.ad25@gmail.com>
2025-01-28 11:39:29 +0200

    01.28

--- ganykla/src/Components/Avis.jsx
+++ ganykla/src/Components/Avis.jsx
@@ -1,6 +1,6 @@
-export default function Avis({id, perbegimas, ganykla, kind}) {
+export default function Avis({id, perbegimas, gardas, kind}) {
    
     return (
-        <div className={kind} onClick={_ => perbegimas(id, ganykla)}>{id}</div>
+        <div className={kind} onClick={_ => perbegimas(id, gardas)}>{id}</div>
     );
 }
