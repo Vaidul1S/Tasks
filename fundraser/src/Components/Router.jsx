c815962c426c9caded95fd2dbcb15927e849d9bb
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-31 10:50:29 +0300

    03.31

--- fundraser/src/Components/Router.jsx
+++ fundraser/src/Components/Router.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, createContext } from 'react';
-import Wraper from '../Components/Wraper';
-import Page404 from '../Components/Page404';
-import Home from "../Pages/Home";
-import Login from '../Pages/Login';
-import Logout from '../Pages/Logout';
+import Wrapper from './Wrapper';
+import Page404 from './Page404';
+import Home from "./Home";
+// import Login from '../Pages/Login';
+// import Logout from '../Pages/Logout';
 
 const RouterContext = createContext();
 
@@ -14,8 +14,8 @@ export const Router = ({ children }) => {
     const routes = {
         '': {c: <Home />, title: 'Home', params: 0},
         'home': {c: <Home />, title: 'Home', params: 0},
-        'login': {c: <Login />, title: 'Login', params: 0, hideNav: true},
-        'logout': {c: <Logout/>, title: 'Logout', params: 0, hideNav: true},
+        // 'login': {c: <Login />, title: 'Login', params: 0, hideNav: true},
+        // 'logout': {c: <Logout/>, title: 'Logout', params: 0, hideNav: true},
         'register': {c: null, title: 'Register', params: 0, hideNav: true},
     };
 
@@ -52,7 +52,7 @@ export const Router = ({ children }) => {
             setShowComponent, 
             routes 
             }}>
-            {showComponent === null ? children : <Wraper>{showComponentList[showComponent] ?? null}</Wraper>}
+            {showComponent === null ? children : <Wrapper>{showComponentList[showComponent] ?? null}</Wrapper>}
         </RouterContext.Provider>
     );
 }
