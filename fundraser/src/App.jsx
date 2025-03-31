722c6b126455f9398260e2eee6f117a4356cce8a
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-31 10:54:35 +0300

    03.31

--- fundraser/src/App.jsx
+++ fundraser/src/App.jsx
@@ -1,6 +1,8 @@
 import { Router } from "./Components/Router";
 import Wrapper from "./Components/Wrapper";
 import Nav from "./Components/Nav";
+import './App.css';
+import './buttons.scss';
 
 export default function App() {
 
