8f08f82f0684d89151c774d053c3e7afefa6f918
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-31 10:44:16 +0300

    03.31

---
+++ fundraser/src/Components/Nav.jsx
@@ -0,0 +1,20 @@
+import Link from "./Link";
+
+export default function Nav() {
+    
+    return (
+        <section className="nav">
+            <div className="container">
+                <nav>
+                    <ul>
+                        <li><img src="" alt="" /></li>
+                        <li><Link to="">Home</Link></li>
+                        <li><Link to="about">About</Link></li>
+                        <li><Link to="contact">Contact</Link></li>
+                    </ul>
+                    
+                </nav>
+            </div>
+        </section>
+    );
+};
