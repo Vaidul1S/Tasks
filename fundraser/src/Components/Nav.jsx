82528c7f7c8740101b10c00838271a2c947cfc22
Vaidul1s <vaidas.ad25@gmail.com>
2025-04-02 10:26:37 +0300

    04.02

--- fundraser/src/Components/Nav.jsx
+++ fundraser/src/Components/Nav.jsx
@@ -1,7 +1,7 @@
 import Link from "./Link";
 
 export default function Nav() {
-    
+
     return (
         <section className="nav">
             <div className="container">
@@ -12,7 +12,10 @@ export default function Nav() {
                         <li><Link to="about">About</Link></li>
                         <li><Link to="contact">Contact</Link></li>
                     </ul>
-                    
+                    <div className="auth">
+                        <Link to="login">Login</Link>
+                        <Link to="register">Register</Link>
+                    </div>
                 </nav>
             </div>
         </section>
