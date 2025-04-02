d507f4e4b8c33cf46b4c871de8e1b4413869c10b
Vaidul1s <vaidas.ad25@gmail.com>
2025-04-02 10:55:47 +0300

    04.02

---
+++ fundraser/src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import RouterContext from "./Router";
+
+export default function Main() {
+ 
+    const { page, parameters, routes } = useContext(RouterContext);
+    console.log('maino parametrai', parameters);
+    
+     
+    const route = _ => {
+ 
+        return routes?.[page]?.c ?? <h1>404</h1>;
+    };
+ 
+    return (
+        <main>
+            {route()}
+        </main>
+    );
+}
