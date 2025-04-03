26f13e6f75a58cac88c3a4b55bfcb9cb9b02c096
Vaidul1s <vaidas.ad25@gmail.com>
2025-04-03 13:23:16 +0300

    04.03

--- fundraser/src/Components/Home.jsx
+++ fundraser/src/Components/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
         <section className="home">
             <div className="container">
                 <h1>Wellcome to Fundraser</h1>
+                <div className="context">
+                    <h2>Content</h2>
+                </div>
                 
             </div>
         </section>
