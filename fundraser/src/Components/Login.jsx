90619c92bc12fb6062c6bb9cb58f27d379593bc6
Vaidul1s <vaidas.ad25@gmail.com>
2025-04-09 11:06:42 +0300

    04.09

--- fundraser/src/Components/Login.jsx
+++ fundraser/src/Components/Login.jsx
@@ -2,26 +2,45 @@ import { useState } from "react";
 import axios from 'axios';
 
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+
+    const [form, setForm] = useState({
+        name: '',
+        password: ''
+    });
+
+    const changeHandler = e => {
+        setForm(f => ({
+            ...f,
+            [e.target.id]: e.target.value
+        }));
+    };
+
+    const redirectAfterLogin = _ => {
+        window.location.hash = '#';
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:3000/login', { username, password })
+        axios.post('http://localhost:3000/login', loginData)
             .then(res => {
-                localStorage.setItem('token', res.data.token);
-                alert('Login successful');                
+                setUser(res.data.user);
+                redirectAfterLogin();                
             })
             .catch(err => alert('Login failed'));
+    };
+
+    const goHome = _ => {
+        window.location.hash = '#';
     };
 
     return (
         <section className="login">
             <h2>Login</h2>
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <input type="text" placeholder="Username" className="p-2 border" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                <input type="password" placeholder="Password" className="p-2 border" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">Login</button>
+                <input type="text" placeholder="Username" className="login_input" value={form.name} onChange={changeHandler}/>
+                <input type="password" placeholder="Password" className="login_input" value={form.password} onChange={changeHandler}/>
+                <button className="button42 lime">Login</button>
+                <button className="button42 red" onClick={goHome}>Cancel</button>
             </form>
         </section>
     );
