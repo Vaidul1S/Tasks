81ab5eb1899fa1a07a60dcc3a340806961895178
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-15 10:49:22 +0300

    04.15

--- fundraser/src/AppJuodrastis.jsx
+++ fundraser/src/AppJuodrastis.jsx
@@ -1,34 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-
-
-function Register() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('http://localhost:5000/register', { username, password })
-      .then(() => alert('Registration successful'))
-      .catch(err => alert('Registration failed'));
-  };
-
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl mb-4">Register</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="text" placeholder="Username" className="p-2 border" value={username} onChange={(e) => setUsername(e.target.value)} required />
-        <input type="password" placeholder="Password" className="p-2 border" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">Register</button>
-      </form>
-    </div>
-  );
-}
-
-export default App;
-
 function AdminPanel() {
   const [stories, setStories] = useState([]);
 
