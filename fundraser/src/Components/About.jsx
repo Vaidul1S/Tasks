b1dcc77163f6f9ea54e05d3408a8eba91ea69040
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-17 10:12:11 +0300

    04.17

--- fundraser/src/Components/About.jsx
+++ fundraser/src/Components/About.jsx
@@ -3,8 +3,9 @@ export default function About() {
     return (
         <section className="about">
             <h1>Achieve your charity goals and help people</h1>
-            <p>Fundraising is the process of seeking and gathering voluntary financial contributions by engaging individuals, businesses, charitable foundations, or governmental agencies to gather money for non-profit organizations.</p>
+            <div className="about_content">
+                <p>Fundraising is the process of seeking and gathering voluntary financial contributions by engaging individuals, businesses, charitable foundations, or governmental agencies to gather money for non-profit organizations.</p>
+            </div>
         </section>
-
     );
 };
