6c9621dc825bde53a1f6af46b09559991a3b24db
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-21 11:33:11 +0300

    04.21

---
+++ fundraser/seeder/users.js
@@ -0,0 +1,9 @@
+import * as icons from './icons.js';
+
+export const users = [
+    { id: 1, name: 'Bebras', role: 'adim' },
+    { id: 2, name: 'Bob', role: 'user' },
+    { id: 3, name: 'Charlie', role: 'user' },
+    { id: 4, name: 'David', role: 'user' },
+    { id: 5, name: 'Eve', role: 'user' }    
+];
