bc9abd0a1c6e54b310d3b5fece345d66290d8d8d
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-22 09:55:38 +0300

    04.22

---
+++ fundraser/fundraiser.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 21, 2025 at 10:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fundraiser`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donations`
+--
+
+CREATE TABLE `donations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `story_id` int(10) UNSIGNED NOT NULL,
+  `donor_name` varchar(32) NOT NULL,
+  `amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `donations`
+--
+
+INSERT INTO `donations` (`id`, `story_id`, `donor_name`, `amount`) VALUES
+(22, 4, 'Gaga', 10000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `expires` date NOT NULL,
+  `token` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `expires`, `token`) VALUES
+(37, 10, '2025-04-17', '5a0e892393e7a62ff54fcf42fc357c86'),
+(43, 1, '2025-04-18', 'f8bbd1404051223789af8f8ef9c6c2bd'),
+(56, 10, '2025-04-19', 'cd4b304b0b8503241cde5d2dc6e476ad');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stories`
+--
+
+CREATE TABLE `stories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(32) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `goal_amount` decimal(10,2) NOT NULL,
+  `collected_amount` decimal(10,2) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `approved` tinyint(1) NOT NULL,
+  `image_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stories`
+--
+
+INSERT INTO `stories` (`id`, `title`, `text`, `goal_amount`, `collected_amount`, `user_id`, `approved`, `image_url`) VALUES
+(4, 'Save children', 'Give us some money and we will look what we can do.', 200000.00, 12000.00, 10, 1, '0'),
+(5, 'Save children again', 'Give us some money and we will look what we can do.', 200000.00, 0.00, 10, 1, '0'),
+(13, 'Help us save Ukrainian pets', 'Fundraising for pets whom lost home and owners because of war. ', 1000000.00, 0.00, 1, 1, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` tinytext NOT NULL,
+  `password` varchar(55) NOT NULL,
+  `role` enum('admin','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `role`) VALUES
+(1, 'Bebras', '202cb962ac59075b964b07152d234b70', 'admin'),
+(2, 'Jonas', '202cb962ac59075b964b07152d234b70', 'user'),
+(3, 'Zebras', '202cb962ac59075b964b07152d234b70', 'user'),
+(5, 'Ona', '202cb962ac59075b964b07152d234b70', 'user'),
+(6, 'Stasys', '202cb962ac59075b964b07152d234b70', 'user'),
+(7, 'Barsukas', '202cb962ac59075b964b07152d234b70', 'user'),
+(8, 'Zose', '202cb962ac59075b964b07152d234b70', 'user'),
+(9, 'Janina', '202cb962ac59075b964b07152d234b70', 'user'),
+(10, 'Juozas', '202cb962ac59075b964b07152d234b70', 'user'),
+(11, 'Lina', '202cb962ac59075b964b07152d234b70', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donations`
+--
+ALTER TABLE `donations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `story_id` (`story_id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `stories`
+--
+ALTER TABLE `stories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donations`
+--
+ALTER TABLE `donations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `stories`
+--
+ALTER TABLE `stories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `donations`
+--
+ALTER TABLE `donations`
+  ADD CONSTRAINT `donations_ibfk_1` FOREIGN KEY (`story_id`) REFERENCES `stories` (`id`);
+
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `stories`
+--
+ALTER TABLE `stories`
+  ADD CONSTRAINT `stories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
