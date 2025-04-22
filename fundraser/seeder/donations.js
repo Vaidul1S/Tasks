dd59eeb730043b6c2697645d92802bed9e806d45
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-22 11:14:53 +0300

    04.22

--- fundraser/seeder/donations.js
+++ fundraser/seeder/donations.js
@@ -1,4 +1,4 @@
 export const donations = [
-    {id: 1, story_id: 1, donor_name: 'Bobute', amount: 1000},
-    {id: 2, story_id: 2, donor_name: 'Bobute', amount: 500},
+    {id: 1, story_id: 1, donor_name: 'Bobute', amount: 10000},
+    {id: 2, story_id: 2, donor_name: 'Bobute', amount: 50000},
 ]
