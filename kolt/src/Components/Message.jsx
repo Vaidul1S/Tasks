a477cf2ab8a863c7fb56eab86b4594f8f05474ae
Vaidul1S <vaidas.ad25@gmail.com>
2025-05-21 12:53:44 +0300

    05.21

--- kolt/src/Components/Message.jsx
+++ kolt/src/Components/Message.jsx
@@ -1,11 +1,11 @@
 export default function Message(messages) {
-    
+
     return (
         <>
             <div className={messages.messages.type}>
                 <p>{messages.messages.title}</p>
                 <p>{messages.messages.text}</p>
-            </div >        
+            </div >
         </>
     );
 };
