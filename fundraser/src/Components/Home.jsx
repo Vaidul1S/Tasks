0e61770603cf645799969dc23e03e69cb1397437
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-14 10:28:55 +0300

    08.14

--- fundraser/src/Components/Home.jsx
+++ fundraser/src/Components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <section className="home">
             <div className="container">
-                <h1 className="home_title">Wellcome to Fundraiser, make someone's day</h1>
+                <h1 className="home_title">A Smallest Care Can Change the Someone's World</h1>
                 <div className="context">
                     <List />
                 </div>
