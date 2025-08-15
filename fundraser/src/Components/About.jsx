bece53f1ba55b2c7e49c52f4578753067efe6c74
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-15 08:05:00 +0300

    08.15

--- fundraser/src/Components/About.jsx
+++ fundraser/src/Components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
         <section className="about">
             <h1>Achieve your charity goals and help people</h1>
             <div className="about_content">
-                <p>For Individuals and charities. No startup fees. No hidden fees. Fundraising is the process of seeking and gathering voluntary financial contributions by engaging individuals, businesses, charitable foundations, or governmental agencies to gather money for non-profit organizations.</p>
+                <p>For Individuals and charities. No startup fees. No hidden fees. Fundraising is the process of seeking and gathering voluntary financial contributions by engaging individuals, businesses, charitable foundations, or governmental agencies to gather money for non-profit organizations. There are thousands of reasons to fundraise—from helping individuals cover their medical bills to nonprofits funding their mission-based programming. No matter the cause, all fundraisers know that the right fundraising idea is the key to success.</p>
             </div>
         </section>
     );
