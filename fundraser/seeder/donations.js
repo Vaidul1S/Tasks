3b335c5429445543049d4109dd24f9b978aafe5c
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-19 11:18:59 +0300

    08.19

--- fundraser/seeder/donations.js
+++ fundraser/seeder/donations.js
@@ -1,4 +1,5 @@
 export const donations = [
-    {id: 1, story_id: 1, donor_name: 'Bobute', amount: 10000},
-    {id: 2, story_id: 2, donor_name: 'Bobute', amount: 50000},
+    {id: 1, story_id: 1, donor_name: 'Fat Joe', amount: 10000},
+    {id: 2, story_id: 2, donor_name: 'Gerasis Samarietis', amount: 50000},
+    {id: 3, story_id: 3, donor_name: 'Poppies Lovers', amount: 5000},
 ]
--- fundraser/seeder/users.js
+++ fundraser/seeder/users.js
@@ -3,10 +3,10 @@ export const users = [
     { id: 2, name: 'Jonas', role: 'user' },
     { id: 3, name: 'Zebras', role: 'user' },
     { id: 5, name: 'Ona', role: 'user' },
-    { id: 6, name: 'Stasys', role: 'user' },
+    { id: 6, name: 'Petras', role: 'user' },
     { id: 7, name: 'Barsukas', role: 'user' },
-    { id: 8, name: 'Zose', role: 'user' },
+    { id: 8, name: 'Tigras', role: 'user' },
     { id: 9, name: 'Janina', role: 'user' },
-    { id: 10, name: 'Juozas', role: 'user' },
+    { id: 10, name: 'Begemotas', role: 'user' },
     { id: 11, name: 'Lina', role: 'user' }
 ];
