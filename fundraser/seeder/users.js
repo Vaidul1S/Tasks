0f5dfe739962d7f3ef20ff3f2c44c117031ea3cf
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-19 11:27:05 +0300

    08.19

--- fundraser/seeder/users.js
+++ fundraser/seeder/users.js
@@ -2,11 +2,8 @@ export const users = [
     { id: 1, name: 'Bebras', role: 'admin' },
     { id: 2, name: 'Jonas', role: 'user' },
     { id: 3, name: 'Zebras', role: 'user' },
-    { id: 5, name: 'Ona', role: 'user' },
+    { id: 5, name: 'Begemotas', role: 'user' },
     { id: 6, name: 'Petras', role: 'user' },
     { id: 7, name: 'Barsukas', role: 'user' },
     { id: 8, name: 'Tigras', role: 'user' },
-    { id: 9, name: 'Janina', role: 'user' },
-    { id: 10, name: 'Begemotas', role: 'user' },
-    { id: 11, name: 'Lina', role: 'user' }
 ];
