6e448966a1228c1c0e9fafea3003e0aa11b3916a
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-25 10:57:46 +0300

    08.25

---
+++ React_pvm/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import './style.scss';
+import Novice from './assets/Novice.jsx';
+
+export default function App() {
+
+
+  return (
+    <>
+      <h1>PVM React</h1>
+      <Novice />
+
+    </>
+  )
+}
+
+
