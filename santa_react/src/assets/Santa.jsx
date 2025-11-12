17bb226ad20b818452d117d302f92c38283dfaf4
Vaidul1S <vaidas.ad25@gmail.com>
2025-11-12 11:48:05 +0200

    11.12

--- santa_react/src/assets/Santa.jsx
+++ santa_react/src/assets/Santa.jsx
@@ -1,61 +1,26 @@
 import { useRef, useState } from "react";
-import { poolRestriction } from "./pool";
+import { poolRestriction } from "./functtions/pool";
+import { pairs } from "./functtions/pairs";
 
 export default function Santa() {
+
+    localStorage.setItem('santa', JSON.stringify(null));
 
     const [pair, setPair] = useState("");
     const [name, setName] = useState("");
     const input = useRef(null);
-
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    };
-
-    function makeSecretSanta(groups) {
-        const people = groups.flat();
-
-        while (true) {
-            const givers = shuffle([...people]);
-            const receivers = shuffle([...people]);
-            const pairs = [];
-            let valid = true;
-
-            for (let i = 0; i < givers.length; i++) {
-                const giver = givers[i];
-                const receiver = receivers[i];
-
-                const groupG = groups.find(g => g.includes(giver));
-                const groupR = groups.find(g => g.includes(receiver));
-
-                if (giver === receiver || groupG === groupR) {
-                    valid = false;
-                    break;
-                }
-                pairs.push([giver, receiver]);
-            }
-
-            if (valid) return pairs;
-        }
-    };
-
-    const pairs = makeSecretSanta(poolRestriction);
-
+    
     const addName = name => {
         setName(n => name);
     }
 
-    function showPair(giver) {
-        let ivestis = giver;
+    const showPair = name => {
+        let ivestis = name;
 
         if (localStorage.getItem('santa').includes(ivestis) || JSON.parse(localStorage.getItem('santa')) == null) {
             pairs.forEach(pair => {
-                if (ivestis == pair[0]) {
-                    result.style.color = 'white';
-                    result.innerHTML = pair[0] + ' -> 🎁 ' + pair[1];
+                if (ivestis == pair[0]) {                    
+                    setPair(pair[0] + ' -> 🎁 ' + pair[1]);
                 }
             });
 
---
+++ santa_react/src/assets/functtions/pairs.js
@@ -0,0 +1,38 @@
+import { poolRestriction } from "./pool";
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+function makeSecretSanta(groups) {
+    const people = groups.flat();
+
+    while (true) {
+        const givers = shuffle([...people]);
+        const receivers = shuffle([...people]);
+        const pairs = [];
+        let valid = true;
+
+        for (let i = 0; i < givers.length; i++) {
+            const giver = givers[i];
+            const receiver = receivers[i];
+
+            const groupG = groups.find(g => g.includes(giver));
+            const groupR = groups.find(g => g.includes(receiver));
+
+            if (giver === receiver || groupG === groupR) {
+                valid = false;
+                break;
+            }
+            pairs.push([giver, receiver]);
+        }
+
+        if (valid) return pairs;
+    }
+};
+
+export const pairs = makeSecretSanta(poolRestriction);
