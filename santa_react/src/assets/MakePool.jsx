564d9aed6b52f05fa8c30c1f20edcaa4246d1936
Vaidul1S <vaidas.ad25@gmail.com>
2025-11-13 11:53:20 +0200

    11.13

--- santa_react/src/assets/MakePool.jsx
+++ santa_react/src/assets/MakePool.jsx
@@ -2,19 +2,39 @@ import { useState } from "react"
 
 export default function MakePool() {
 
-    const [fams, setFams] = useState(0);
+    const [fams, setFams] = useState([]);
 
-    const makePool = _ => {
-        
-    }
+    const famNumb = (count) => {
+        const newFam = Array.from({ length: count }, (_, i) => families[i] || []);
+        setFams(newFam);
+    };
+
+    const members = (famIndex, count) => {
+        setFamilies((prev) => {
+            const newFam = [...prev];
+            newFamilies[famIndex] = Array.from(
+                { length: count },
+                (_, i) => newFam[famIndex]?.[i] || ""
+            );
+            return newFam;
+        });
+    };
+
+    const membersNames = (famIndex, memberIndex, name) => {
+        setFamilies((prev) => {
+            const newFam = [...prev];
+            newFam[famIndex][memberIndex] = name;
+            return newFam;
+        });
+    };
 
     return (
         <>
-        <div>
-            <h1>Make array</h1>
-            <input type="text" />
-            <button type="submit" className="button27 blue"></button>
-        </div>
+            <div>
+                <h1>Make Pool</h1>
+                <input type="text" placeholder="Enter a number of families" value={fams} />
+                <button type="submit" className="button27 blue" onClick={_ => setFams(famsNumb)}></button>
+            </div>
         </>
     )
 }
