cd4016c3675a131abcee9d39e81c392730b17b2c
Vaidul1S <vaidas.ad25@gmail.com>
2026-02-12 21:19:00 +0200

    02.12

--- fundraiser/seeder/donations.js
+++ fundraiser/seeder/donations.js
@@ -1,5 +1,5 @@
 export const donations = [
-    {id: 1, story_id: 1, donor_name: 'Fat Joe', amount: 12345},
-    {id: 2, story_id: 2, donor_name: 'Gerasis Samarietis', amount: 54321},
-    {id: 3, story_id: 3, donor_name: 'Pooppies Lovers', amount: 4989},
+    { id: 1, story_id: 1, donor_name: 'Fat Joe', amount: 12345 },
+    { id: 2, story_id: 2, donor_name: 'Gerasis Samarietis', amount: 54321 },
+    { id: 3, story_id: 3, donor_name: 'Pooppies Lovers', amount: 4989 },
 ];
