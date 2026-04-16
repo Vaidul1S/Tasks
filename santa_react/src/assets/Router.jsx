fb9253070b0ceda10e0e62a316267e0952cb48b3
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-16 11:24:07 +0300

    04.16

--- santa_react/src/assets/Router.jsx
+++ santa_react/src/assets/Router.jsx
@@ -10,49 +10,49 @@ const RouterContext = createContext();
 export const Router = ({ children }) => {
 
     const showComponentList = { error404: <Page404 /> };
-    
-        const routes = {
-            '': {c: <Home />, title: 'Home', params: 0},
-            'santa': {c: <Santa />, title: 'Santa', params: 0},
-            'makePool': {c: <MakePool/>, title: 'MakePool', params: 0},
-        };
-    
-        const [page, setPage] = useState(_ => {
+
+    const routes = {
+        '': { c: <Home />, title: 'Home', params: 0 },
+        'santa': { c: <Santa />, title: 'Santa', params: 0 },
+        'makePool': { c: <MakePool />, title: 'MakePool', params: 0 },
+    };
+
+    const [page, setPage] = useState(_ => {
+        let hash = window.location.hash.replace('#', '');
+        hash = hash.split('/').shift();
+        return hash;
+    });
+    const [parameters, setParameters] = useState(_ => {
+        let hash = window.location.hash.replace('#', '');
+        hash = hash.split('/').shift();
+        return hash;
+    });
+
+    const [showComponent, setShowComponent] = useState(null);
+
+    useEffect(_ => {
+        window.addEventListener('hashchange', _ => {
             let hash = window.location.hash.replace('#', '');
-            hash = hash.split('/').shift();
-            return hash;
+            hash = hash.split('/');
+            setPage(hash.shift());
+            setParameters(hash);
         });
-        const [parameters, setParameters] = useState(_ => {
-            let hash = window.location.hash.replace('#', '');
-            hash = hash.split('/').shift();
-            return hash;
-        });
-    
-        const [showComponent, setShowComponent] = useState(null);
-    
-        useEffect(_ => {
-            window.addEventListener('hashchange', _ => {
-                let hash = window.location.hash.replace('#', '');
-                hash = hash.split('/');
-                setPage(hash.shift());
-                setParameters(hash);
-            });
-        }, []);
-    
-        useEffect(_ => {
-            setShowComponent(null);
-        },[parameters, page])
-    
-        return (
-            <RouterContext.Provider value={{
-                page, 
-                parameters, 
-                setShowComponent, 
-                routes 
-                }}>
-                {showComponent === null ? children : <Wraper>{showComponentList[showComponent] ?? null}</Wraper>}
-            </RouterContext.Provider>
-        );
-    }
-    
-    export default RouterContext;
+    }, []);
+
+    useEffect(_ => {
+        setShowComponent(null);
+    }, [parameters, page])
+
+    return (
+        <RouterContext.Provider value={{
+            page,
+            parameters,
+            setShowComponent,
+            routes
+        }}>
+            {showComponent === null ? children : <Wraper>{showComponentList[showComponent] ?? null}</Wraper>}
+        </RouterContext.Provider>
+    );
+}
+
+export default RouterContext;
