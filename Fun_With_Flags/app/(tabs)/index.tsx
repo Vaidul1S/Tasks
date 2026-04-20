bbd1a9695ece37dcbf8e45385fc0b8f49ccc4345
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-20 12:01:44 +0300

    04.20

---
+++ Fun_With_Flags/app/(tabs)/index.tsx
@@ -0,0 +1,48 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/flag_map.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Fun With Flags!</ThemedText>
+        
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Guess a Country</ThemedText>
+        <ThemedText type="defaultSemiBold">Guess a country by flag from four choices.</ThemedText>
+
+      </ThemedView>
+
+
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 580,    
+    top: 0,
+    left: 0,    
+  },
+});
