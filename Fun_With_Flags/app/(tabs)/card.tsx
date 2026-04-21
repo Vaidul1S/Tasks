9e3f76dd198ce7305df7b193972e1c8dbae26f84
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-21 23:32:34 +0300

    04.21

--- Fun_With_Flags/app/(tabs)/card.tsx
+++ Fun_With_Flags/app/(tabs)/card.tsx
@@ -1,6 +1,9 @@
+import { Image } from 'expo-image';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+
 export default function card(){
 
     return (
-        <></>
+        <Image/>
     );
 }
