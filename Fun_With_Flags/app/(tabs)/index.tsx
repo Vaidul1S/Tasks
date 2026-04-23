281fd50dd189fdc089c9f58ae8919157d32928b7
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-23 13:23:38 +0300

    04.23

--- Fun_With_Flags/app/(tabs)/index.tsx
+++ Fun_With_Flags/app/(tabs)/index.tsx
@@ -30,7 +30,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',    
+    flexDirection: 'column',    
     gap: 8,
     
   },
