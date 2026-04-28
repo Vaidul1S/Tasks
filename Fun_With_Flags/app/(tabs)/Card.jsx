91d0a3680e237ca51a7b19eebe51a5a1ac491523
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-28 11:11:01 +0300

    04.28

--- Fun_With_Flags/app/(tabs)/Card.jsx
+++ Fun_With_Flags/app/(tabs)/Card.jsx
@@ -3,6 +3,7 @@ import { countries } from './countries';
 import { ThemedView } from '@/components/themed-view';
 import { ThemedText } from '@/components/themed-text';
 import { ScrollView } from 'react-native';
+import { emoji } from './emoji_flags';
 
 export default function Card() {
 
@@ -10,7 +11,9 @@ export default function Card() {
         <ScrollView headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}>
             <ThemedView>
                 <Image />
+                <ThemedText>"flag":"🇦🇫"</ThemedText>
                 <ThemedText>{countries.map(c => "\t" + c + "\n ")}</ThemedText>
+                
             </ThemedView>
         </ScrollView>
     );
