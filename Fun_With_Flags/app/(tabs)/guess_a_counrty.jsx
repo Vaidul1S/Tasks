1ca82f279e74f9fa892794d5b8f13d389db6f3af
Vaidul1S <vaidas.ad25@gmail.com>
2026-04-29 21:03:06 +0300

    04.29

--- Fun_With_Flags/app/(tabs)/guess_a_counrty.jsx
+++ Fun_With_Flags/app/(tabs)/guess_a_counrty.jsx
@@ -1,10 +1,16 @@
 import { ScrollView } from "react-native";
+import { Image } from 'expo-image';
+import { ThemedText } from '@/components/themed-text';
 
 export default function guess_a_country(){
 
     return(
         <ScrollView>
-            
+            <Image/>
+            <ThemedText clasname={option1}>1. </ThemedText>
+            <ThemedText clasname={option2}>2. </ThemedText>
+            <ThemedText clasname={option3}>3. </ThemedText>
+            <ThemedText clasname={option4}>4. </ThemedText>
         </ScrollView>
     )
 }
