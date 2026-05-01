774777ee226d344912b56ebd6f993a3f6b6e1bfa
Vaidul1S <vaidas.ad25@gmail.com>
2026-05-01 11:42:20 +0300

    05.01

--- Fun_With_Flags/app/(tabs)/game.jsx
+++ Fun_With_Flags/app/(tabs)/game.jsx
@@ -1,6 +1,5 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity, Text } from "react-native";
 import { Image } from 'expo-image';
-import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { useState } from "react";
 import { flags } from '../../assets/data/flags.js';
@@ -8,6 +7,7 @@ import { flags } from '../../assets/data/flags.js';
 export default function game() {
 
     const [score, setScore] = useState(0);
+    const [mistakes, setMistakes] = useState(0);
 
     let pick = Math.floor(Math.random() * flags.length);
     const flag = flags[pick];
@@ -17,18 +17,25 @@ export default function game() {
     let options = [flag.name, flags[option1].name, flags[option2].name, flags[option3].name];
     options.sort(function(){return 0.5 - Math.random()});
 
+    const submitGuess = e =>{
+        if (e == flag.name){
+            setScore(s => s + 1);
+        } else {
+            setMistakes(m => m + 1);
+        }
+    }
     return (
         <ScrollView >
             <ThemedView style={styles.body}>
                 <ScrollView style={styles.container}>
-                    <Image style={{width: 380, height: 200}} source={{ uri: flag.flag, }} resizeMode={'cover'}/>
+                    <Image style={{width: 380, height: 220}} source={{ uri: flag.flag, }} resizeMode={'cover'}/>
                 </ScrollView>
 
-                <ThemedText style={styles.option}>1. {options[0]}</ThemedText>
-                <ThemedText style={styles.option}>2. {options[1]}</ThemedText>
-                <ThemedText style={styles.option}>3. {options[2]}</ThemedText>
-                <ThemedText style={styles.option}>4. {options[3]}</ThemedText>
-                <ThemedText style={styles.score}>Score: {score}</ThemedText>
+                <TouchableOpacity onPress={_=> submitGuess(options[0])}><Text style={styles.option}>1. {options[0]}</Text></TouchableOpacity>
+                <TouchableOpacity onPress={_=> submitGuess(options[1])}><Text style={styles.option}>2. {options[1]}</Text></TouchableOpacity>
+                <TouchableOpacity onPress={_=> submitGuess(options[2])}><Text style={styles.option}>3. {options[2]}</Text></TouchableOpacity>
+                <TouchableOpacity onPress={_=> submitGuess(options[3])}><Text style={styles.option}>4. {options[3]}</Text></TouchableOpacity>
+                <Text style={styles.score}>Score: {score}</Text>
 
             </ThemedView>
         </ScrollView>
@@ -38,6 +45,7 @@ export default function game() {
 const styles = StyleSheet.create({
     body: {
         alignSelf: 'center',
+        color: 'white',
     },
     container: {
         flex: 1,        
@@ -49,9 +57,11 @@ const styles = StyleSheet.create({
         fontsize: 20,
         margin: (0, 15),
         paddingLeft: 30,
+        border: '1px',
+        borderRadius: '50%',
     },
     score: {
-        fontsize: 40,
+        fontsize: 60,
         alignSelf: 'flex-end',
         margin: 30,
     },
