d5c91b704dfb54905806f59ff4bf0166880320d7
Vaidul1S <vaidas.ad25@gmail.com>
2026-05-04 10:51:58 +0300

    05.04

--- Fun_With_Flags/app/(tabs)/game.jsx
+++ Fun_With_Flags/app/(tabs)/game.jsx
@@ -1,14 +1,18 @@
-import { ScrollView, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity, Text, Modal } from "react-native";
 import { Image } from 'expo-image';
 import { ThemedView } from '@/components/themed-view';
 import { useState } from "react";
 import { flags } from '../../assets/data/flags.js';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function game() {
 
     const [score, setScore] = useState(0);
     const [guess, setGuess] = useState('Take a guess');
     const [question, setQuestion] = useState(1);
+    const [gameOn, setGameOn] = useState(false);
+    const [lives, setLives] = useState(0);   
+    const [gameOver, setGameOver] = useState(false); 
 
     let pick = Math.floor(Math.random() * flags.length);
     const flag = flags[pick];
@@ -18,39 +22,93 @@ export default function game() {
     let options = [flag.name, flags[option1].name, flags[option2].name, flags[option3].name];
     options.sort(function () { return 0.5 - Math.random() });
 
-
     const submitGuess = e => {
         if (e == flag.name) {
             setScore(s => s + 1);
             setGuess('Correct!');
         } else {
             setGuess('Wrong!');
+            if(lives > 0){
+                setLives(l => l - 1);
+            }
         }
         setQuestion(q => q + 1);
-    }
+    };
+
+    const startTheGame = e => {
+        if(e == 20){
+            setQuestion(20);
+            setGameOn(true);
+        } else if (e == 50){
+            setQuestion(50);
+            setGameOn(true);
+        } else if (e == 3){
+            setLives(3);
+            setGameOn(true);
+        } else if (e == 5){
+            setLives(5);
+            setGameOn(true);
+        }
+    };
+
     return (
-        <ScrollView >
-            <ThemedView style={styles.body}>
-                <Text style={styles.score}>Guess a Country!</Text>
-                <Text style={styles.question}>Question #{question}</Text>
-
-                <ScrollView style={styles.container}>
-                    <Image style={{ width: 380, height: 220 }} source={{ uri: flag.flag, }} resizeMode={'contain'} />
-                </ScrollView>
-
-                <TouchableOpacity onPress={_ => submitGuess(options[0])}><Text style={styles.option}>1. {options[0]}</Text></TouchableOpacity>
-                <TouchableOpacity onPress={_ => submitGuess(options[1])}><Text style={styles.option}>2. {options[1]}</Text></TouchableOpacity>
-                <TouchableOpacity onPress={_ => submitGuess(options[2])}><Text style={styles.option}>3. {options[2]}</Text></TouchableOpacity>
-                <TouchableOpacity onPress={_ => submitGuess(options[3])}><Text style={styles.option}>4. {options[3]}</Text></TouchableOpacity>
-                <Text style={guess == 'Correct!' ? styles.guess1 : styles.guess2}>{guess}</Text>
-                <Text style={styles.score}>Score: {score}</Text>
+        <SafeAreaProvider>
+            <ThemedView style={styles.game}>
+                <Text style={styles.title}>Shall we play a game?</Text>
+                <TouchableOpacity onPress={_=> startTheGame(20)} style={styles.menu}>Game of 20</TouchableOpacity>
+                <TouchableOpacity onPress={_=> startTheGame(50)} style={styles.menu}>Game of 50</TouchableOpacity>
+                <TouchableOpacity onPress={_=> startTheGame(3)} style={styles.menu}>3 Lives</TouchableOpacity>
+                <TouchableOpacity onPress={_=> startTheGame(5)} style={styles.menu}>5 Lives</TouchableOpacity>
 
             </ThemedView>
-        </ScrollView>
+
+            <Modal visible={gameOn} style={styles.modal} >
+                <ThemedView style={styles.body}>
+                    <Text style={styles.lives}>Lives: {lives}</Text>
+                    <Text style={styles.score}>Guess a Country!</Text>
+                    <Text style={styles.question}>Question #{question}</Text>
+
+                    <ScrollView style={styles.container}>
+                        <Image style={{ width: 380, height: 220 }} source={{ uri: flag.flag, }} resizeMode={'contain'} />
+                    </ScrollView>                    
+                    <TouchableOpacity onPress={_ => submitGuess(options[0])}><Text style={styles.option}>1. {options[0]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={_ => submitGuess(options[1])}><Text style={styles.option}>2. {options[1]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={_ => submitGuess(options[2])}><Text style={styles.option}>3. {options[2]}</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={_ => submitGuess(options[3])}><Text style={styles.option}>4. {options[3]}</Text></TouchableOpacity>
+                    <Text style={guess == 'Correct!' ? styles.guess1 : styles.guess2}>{guess}</Text>                    
+                    <Text style={styles.score}>Score: {score}</Text>
+
+                </ThemedView>
+            </Modal>
+        </SafeAreaProvider>
     )
 }
 
 const styles = StyleSheet.create({
+    game: {
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontFamily: 'monospace',
+        fontSize: '44px',
+        color: 'white',        
+        margin: 30,
+    },
+    menu: {
+        fontFamily: 'monospace',
+        fontSize: '24px',
+        color: 'white',
+        margin: (0, 20),
+        textAlign: 'center',
+        border: '1.5px, dashed, #ffffff80',
+        borderRadius: '25px',
+        padding: 10
+    },
+    modal: {
+        flex: 1,
+    },
     body: {
         alignSelf: 'center',
     },
@@ -75,7 +133,7 @@ const styles = StyleSheet.create({
         fontSize: '40px',
         color: 'white',
         alignSelf: 'flex-end',
-        margin: 30,        
+        margin: 30,
     },
     guess1: {
         fontFamily: 'monospace',
@@ -99,5 +157,12 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: 'center',
         paddingBottom: 8,
+    },
+    lives:{
+        fontFamily: 'monospace',
+        fontSize: '30px',
+        color: 'lime',
+        alignSelf: 'flex-start',
+        padding: (40, 20),
     },
 })
