f492e15413c66ac677508a7d41b0e56a271ece6f
Vaidul1S <vaidas.ad25@gmail.com>
2026-05-22 10:24:07 +0300

    05.22

--- Fun_With_Flags/app/(tabs)/sheet.jsx
+++ Fun_With_Flags/app/(tabs)/sheet.jsx
@@ -23,10 +23,10 @@ export default function sheet() {
 }
 
 const styles = StyleSheet.create({
-    sheet: {        
+    sheet: {
         alignSelf: 'center',
         padding: 10,
-        width: 410,        
+        width: 410,
         backgroundColor: '#446b7762',
     },
     list: {
