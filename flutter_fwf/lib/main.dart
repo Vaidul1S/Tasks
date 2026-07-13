5401377dda39beb63ab913620331652bcb6b30f5
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-13 11:39:34 +0300

    07.13

--- flutter_fwf/lib/main.dart
+++ flutter_fwf/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fwf/navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,13 +24,15 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text('Fun with Flags')),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.copyright_rounded), label: 'Credits'),
-          ],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Fun with Flags',
+              style: TextStyle(color: Color.fromRGBO(135, 8, 139, 1), fontFamily: 'Papyrus'),
+            ),
+          ),
         ),
+        bottomNavigationBar: NavBarWidget(),
       ),
     );
   }
---
+++ flutter_fwf/lib/navbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NavBarWidget extends StatefulWidget {
+  const NavBarWidget({super.key});
+
+  @override
+  State<NavBarWidget> createState() => _NavBarWidgetState();
+}
+
+class _NavBarWidgetState extends State<NavBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.copyright_rounded),
+              label: 'Credits',
+            ),
+          ],
+          selectedIndex: 0,
+        );
+  }
+}
